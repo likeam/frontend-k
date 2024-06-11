519ca957e14ea6569a51f0dfe4487be9397213d9
Abdul Rehamn <likeam99@gmail.com>
2024-06-11 20:42:01 +0500

    Building Shop Category Section

--- src/Components/NewsLetter/NewsLetter.jsx
+++ src/Components/NewsLetter/NewsLetter.jsx
@@ -1,16 +1,16 @@
-import "./NewsLetter.css"
+import "./NewsLetter.css";
 
 const NewsLetter = () => {
   return (
     <div className="newsletter">
-        <h1>Get Exclusive Offers On Your Email</h1>
-        <p>Subscribe to our newsletter and get updated</p>
+      <h1>Get Exclusive Offers On Your Email</h1>
+      <p>Subscribe to our newsletter and get updated</p>
+      <div>
         <input type="email" placeholder="Enter your email address" />
-     
-       <button>Subscribe</button>
-       
+        <button>Subscribe</button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default NewsLetter
+export default NewsLetter;
