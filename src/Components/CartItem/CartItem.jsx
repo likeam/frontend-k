8e74b4456dd4085ca9fb31a576305b2f3af0463b
Abdul Rehamn <likeam99@gmail.com>
2024-06-15 08:42:31 +0500

    Building cart items

---
+++ src/Components/CartItem/CartItem.jsx
@@ -0,0 +1,47 @@
+import { useContext } from "react";
+import "./CartItem.css";
+import { ShopContext } from "../../Context/ShopContext";
+import romoveIcon from "../assets/cart_cross_icon.png";
+
+const CartItem = () => {
+  const { all_product, cartItems, addToCart, removeFromCart } =
+    useContext(ShopContext);
+
+  return (
+    <div className="cartitem">
+      <div className="cartitem-format-main">
+        <p>Products</p>
+        <p>Title</p>
+        <p>Price</p>
+        <p>Quantity</p>
+        <p>Total</p>
+        <p>Remove</p>
+      </div>
+      <hr />
+      {all_product.map((e) => {
+        if (cartItems[e.id] > 0) {
+          return (
+            <div>
+              <div className="cartitem-format">
+                <img src={e.image} alt="" className="carticon-product-icon" />
+                <p>{e.name}</p>
+                <p>Rs {e.new_price}</p>
+                <button className="catitem-quantity">{cartItems[e.id]}</button>
+                <p>Rs {e.new_price*cartItems[e.id]}</p>
+                <img
+                  src={romoveIcon}
+                  onClick={() => removeFromCart(e.id)}
+                  alt=""
+                  className=""
+                />
+              </div>
+              <hr />
+            </div>
+          );
+        }
+      })}
+    </div>
+  );
+};
+
+export default CartItem;
--- src/Pages/Cart.jsx
+++ src/Pages/Cart.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import CartItem from '../Components/CartItem/CartItem'
 
 const Cart = () => {
   return (
-    <div>Cart</div>
+    <>
+      <CartItem />
+    </>
   )
 }
 
