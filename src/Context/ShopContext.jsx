9dab4fc3f101d64a2302b62957d1b959456114c3
Abdul Rehamn <likeam99@gmail.com>
2024-06-20 10:01:34 +0500

    Removing error

--- src/Context/ShopContext.jsx
+++ src/Context/ShopContext.jsx
@@ -21,11 +21,14 @@ const ShopContextProvider = (props) => {
     const removeFromCart = (itemId) => {
         setCartItems((prev) => ({...prev,[itemId]: prev[itemId]-1}))
     }
-    const getTotalCartAmount = (itemId) =>{
+    const getTotalCartAmount = () =>{
         let totalAmount = 0
-        for(const item  cartItems){
-           
-
+        for(const item in  cartItems ){
+           if(cartItems[item]>0){
+                let itemInfo = all_product.find((product) => product.id===Number(item))
+                totalAmount += itemInfo.new_price * cartItems[item]
+           }
+           return totalAmount
         }
     }
 
